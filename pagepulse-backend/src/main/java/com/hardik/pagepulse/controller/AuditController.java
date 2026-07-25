75d33aee929a36c11c8e1c72e621dbdbe8814c8d
hardik <sainihardik638@gmail.com>
2026-07-25 15:54:31 +0530

    cors fix

--- pagepulse-backend/src/main/java/com/hardik/pagepulse/controller/AuditController.java
+++ pagepulse-backend/src/main/java/com/hardik/pagepulse/controller/AuditController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/audit")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = {"http://localhost:5173", "https://page-pulse-ui.onrender.com"})
 public class AuditController {
     private final AuditService auditService;
 
