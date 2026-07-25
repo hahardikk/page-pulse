42aea17d988af622134d49dd8a8aadcc84e81fd7
hardik <sainihardik638@gmail.com>
2026-07-25 15:49:32 +0530

    env file commit

--- page-pulse-ui/src/services/auditService.js
+++ page-pulse-ui/src/services/auditService.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api/audit/post";
+
+const API_URL = import.meta.env.VITE_API_URL;
 
 export const auditWebsite = async (url) => {
   const response = await axios.post(API_URL, {
